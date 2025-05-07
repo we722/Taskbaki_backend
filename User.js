14ee18cd8b2c53d5cbe43385ec28a82f3b73c941
we722 <shaminroy681@gmail.com>
2025-05-07 10:48:37 +0530

    Update User.js

--- User.js
+++ User.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  points: { type: Number, default: 0 }
+  points: { type: Number, default: 0 },
+  balanceINR: { type: Number, default: 0 } // নতুন ফিল্ড INR ব্যালেন্সের জন্য
 });
 
 module.exports = mongoose.model("User", userSchema);
