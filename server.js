be55091b17bad8a9c9460d92bbea501063bfcebd
we722 <shaminroy681@gmail.com>
2025-05-07 22:25:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 require('dotenv').config();
-
+const express = require('express');
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
 
-const jwtSecret = process.env.JWT_SECRET;
+const app = express();
+
+// Middleware (যদি দরকার হয়)
+app.use(express.json());
+
+// Database connection
+mongoose.connect(process.env.MONGO_URI, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
+// Example route
+app.get('/', (req, res) => {
+  res.send('Taskbaki backend is running');
+});
+
+// Start server
 const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
